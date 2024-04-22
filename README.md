4fe2682f9f63db9a4262e7671f820ac00b2c6032
GuglioIsStupid <bradsbluetab@gmail.com>
2024-04-21 20:17:35 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CrashButton
+
+Literally creates a button that forcefully crashes your Geometry Dash.
---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+#include <Geode/modify/MenuLayer.hpp>
+
+void CrashThisGame() {
+	*(int*)0 = 0;
+}
+
+class $modify(MyMenuLayer, MenuLayer) {
+	bool init() {
+		if (!MenuLayer::init()) {
+			return false;
+		}
+
+		auto crashButton = CCMenuItemSpriteExtra::create(
+			CCSprite::createWithSpriteFrameName("GJ_dislikeBtn_001.png"),
+			this,
+			menu_selector(MyMenuLayer::onCrashButton)
+		);
+
+		auto menu = this->getChildByID("bottom-menu");
+		menu->addChild(crashButton);
+
+		crashButton->setID("my-button"_spr);
+
+		menu->updateLayout();
+
+		return true;
+	}
+
+	void onCrashButton(CCObject*) {
+		CrashThisGame();
+	}
+};
